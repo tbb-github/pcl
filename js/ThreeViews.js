b1cf4371d7103d6ac893f5be0e3cb162efe2d267
涂兵兵 <2653038230@qq.com>
2024-11-03 22:47:43 +0800

    三视图

--- js/ThreeViews.js
+++ js/ThreeViews.js
@@ -2,13 +2,13 @@
 import {UIDiv} from '../libs/ui.js'
 import * as THREE from 'three';
 
-function ThreeViews(viewsConfig, renderer, parent, cube) {
+function ThreeViews(viewsConfig, parent, cube) {
 	const container = new UIDiv();
 	parent.appendChild(container.dom);
 	container.setId('threeViews');
 	container.setClass('three-views');
 	container.setInnerHTML(`
-		<div id="topView">
+		<div id="topView" tabindex="-1" class="view-manipulator">
 			<svg id="topViewSvg"  xmlns="http://www.w3.org/2000/svg">
 				<line class="svg-line" id="topLineLeft"></line>
 				<rect width="20" height="140" class="line-handle ew-handle" id="topLineLeftHandle"/>
@@ -29,7 +29,7 @@ function ThreeViews(viewsConfig, renderer, parent, cube) {
 			
 
 		</div>
-		<div id="rightView">
+		<div id="rightView" tabindex="-1" class="view-manipulator" >
 			<svg id="rightViewSvg" xmlns="http://www.w3.org/2000/svg">
 				<line class="svg-line" id="rightLineLeft"></line>
 				<rect width="20" height="140" class="line-handle ew-handle" id="rightLineLeftHandle"/>
@@ -48,7 +48,7 @@ function ThreeViews(viewsConfig, renderer, parent, cube) {
 				<rect width="40" height="40"  class="line-handle grab-handle" id="rightLineMoveHandle"/>
 			</svg>
 		</div>
-		<div id="frontView">
+		<div id="frontView" tabindex="-1" class="view-manipulator">
 			<svg id="frontViewSvg" xmlns="http://www.w3.org/2000/svg">
 			    <line class="svg-line" id="frontLineLeft"></line>
 				<rect width="20" height="140" class="line-handle ew-handle" id="frontLineLeftHandle"/>
@@ -94,7 +94,7 @@ function initThreeCamera(viewsConfig) {
  * 
  * @param {*} param0 
  */
-function updateThreeCameraByView(viewsConfig, {scale, rotation, position}) {
+function updateThreeCameraByView(viewsConfig, {scale}) {
 	let boxRatio;
 	for (let i = 0; i < viewsConfig.length; i++) {
 		const view = viewsConfig[i];
@@ -120,6 +120,7 @@ function updateThreeCameraByView(viewsConfig, {scale, rotation, position}) {
 		}
 		if (cameraWidth/cameraHeight > view.aspect) {
 			cameraHeight = cameraWidth /view.aspect;
+			
 		} else {
 			cameraWidth = cameraHeight * view.aspect;
 		}
@@ -138,8 +139,7 @@ function updateThreeCameraByView(viewsConfig, {scale, rotation, position}) {
  * 
  * @param {*} param0 
  */
-function threeCameraLookAtCube(viewsConfig, {position, rotation, scale}) {
-
+function threeCameraLookAtCube(viewsConfig, {position, rotation}) {
 	const cameraTop = viewsConfig[0].camera;
 	cameraTop.position.set(position.x, position.y, position.z);
 	cameraTop.rotation.set(rotation.x, rotation.y, rotation.z);
@@ -168,8 +168,8 @@ function initViewsEventListener(viewsConfig,cube) {
 		const viewConfig = viewsConfig[i];
 		const type = viewConfig.type;
 		initViewWheelListener(viewConfig, type, viewsConfig, cube);//初始化view鼠标缩放事件
-
-
+		initKeydonwListener(type, cube, viewsConfig);//初始化view键盘事件
+		initMouseListener(type);//初始化鼠标移入移出事件
 
 		const lineLeft = document.getElementById(type+'LineLeft');
 		const lineRight = document.getElementById(type+'LineRight');
@@ -189,60 +189,40 @@ function initViewsEventListener(viewsConfig,cube) {
 		const handleMove = document.getElementById(type+'LineMoveHandle');
 		const handleDirection = document.getElementById(type+'DirectionHandle');
 	
-		initHandleListener(handleLeft, linesArr, 'edge');//初始化句柄左边界（矩形）监听事件
-		initHandleListener(handleRight, linesArr, 'edge');//初始化句柄右边界（矩形）监听事件
-		initHandleListener(handleTop, linesArr, 'edge');//初始化句柄顶部边界（矩形）监听事件
-		initHandleListener(handleBottom, linesArr, 'edge');//初始化句柄底部边界（矩形）监听事件
-		initHandleListener(handleTopLeft, linesArr, 'edge');//初始化句柄顶部左边角界（矩形）监听事件
-		initHandleListener(handleTopRight, linesArr, 'edge');//初始化句柄顶部右边角界（矩形）监听事件
-		initHandleListener(handleBottomLeft, linesArr, 'edge');//初始化句柄底部左边角界（矩形）监听事件
-		initHandleListener(handleBottomRight, linesArr, 'edge');//初始化句柄底部右边角界（矩形）监听事件
-		initHandleListener(handleMove, linesArr, 'move');//初始化句柄移动中心（矩形）监听事件
-		initHandleListener(handleDirection, linesArr, 'direction');//初始化句柄方向边界（矩形）监听事件
-        // installHanler(handle.move, null , 'move');
 
-		// let startPos = {};
-		// let endPos = {};
-		// view.addEventListener('mousedown', (event)=>{
-		// 	event.stopPropagation();
-		// 	startPos.x = event.clientX;
-		// 	startPos.y = event.clientY;
-		// 	console.log(startPos, 'startPos');
-		// 	highlightLine(type, linesArr);
-		// })
-		// view.addEventListener('mouseup', (event)=>{
-		// 	event.stopPropagation();
-		// 	endPos.x = event.clientX;
-		// 	endPos.y = event.clientY;
-		// 	let deltaX = endPos.x - startPos.x;
-		// 	let deltaY = endPos.y - startPos.y;
-		// 	let angle = Math.atan2(deltaY, deltaX);
-		// 	if(type === 'top') {
-		// 		cube.rotation.y += angle
-		// 	} else if(type === 'right') {
-		// 		cube.rotation.x += angle
-		// 	} else {
-		// 		cube.rotation.z += angle
-		// 	}
-		// 	updateCamera(cube)
-		// 	renderViews.forEach(v=>{
-		// 		updateViewBySize(v, cube);
-		// 	})
-		
-		// 	console.log(event, deltaY, deltaX, angle,'angle');
-		// 	// hideLine(type, linesArr);
-		// })
-		// lineDirection.addEventListener('mousedown', (event)=>{
+		const svg = document.getElementById(type+"ViewSvg");
+
+
+		initHandleListener(svg, handleLeft, linesArr, 'edge', {x:-1,y:0}, viewConfig, cube, viewsConfig);//初始化句柄左边界（矩形）监听事件
+		initHandleListener(svg, handleRight, linesArr, 'edge', {x:1,y:0}, viewConfig, cube, viewsConfig);//初始化句柄右边界（矩形）监听事件
+		initHandleListener(svg, handleTop, linesArr, 'edge', {x:0,y:1}, viewConfig, cube, viewsConfig);//初始化句柄顶部边界（矩形）监听事件
+		initHandleListener(svg, handleBottom, linesArr, 'edge', {x:0,y:-1}, viewConfig, cube, viewsConfig);//初始化句柄底部边界（矩形）监听事件
+		initHandleListener(svg, handleTopLeft, linesArr, 'edge', {x:-1,y:1}, viewConfig, cube, viewsConfig);//初始化句柄顶部左边角界（矩形）监听事件
+		initHandleListener(svg, handleTopRight, linesArr, 'edge', {x:1,y:1}, viewConfig, cube, viewsConfig);//初始化句柄顶部右边角界（矩形）监听事件
+		initHandleListener(svg, handleBottomLeft, linesArr, 'edge', {x:1,y:-1}, viewConfig, cube, viewsConfig);//初始化句柄底部左边角界（矩形）监听事件
+		initHandleListener(svg, handleBottomRight, linesArr, 'edge', {x:-1,y:-1}, viewConfig, cube, viewsConfig);//初始化句柄底部右边角界（矩形）监听事件
+		initHandleListener(svg, handleMove, linesArr, 'move',null, viewConfig, cube, viewsConfig);//初始化句柄移动中心（矩形）监听事件
+		initHandleListener(svg, handleDirection, linesArr, 'direction', null, viewConfig, cube, viewsConfig);//初始化句柄方向边界（矩形）监听事件
 	
-		// 	event.stopPropagation();
-		// 	highlightLine(type, linesArr);
-		// })
-
-		// view.addEventListener('mouseenter', ()=>{highlightLine(type, linesArr)});
-		// view.addEventListener('mouseleave', ()=>{hideLine(type, linesArr)});
-		// initLineEventListener(type, 'LineDirection')
+		
 	}
 
+}
+
+function initKeydonwListener(type, cube,viewsConfig) {
+	const view = document.getElementById(type+"View");
+	view.addEventListener('keydown', function(event) {
+		onKeyDown(event, type, cube,viewsConfig);
+	  });
+}
+function initMouseListener(type) {
+	const view = document.getElementById(type+"View");
+	view.onmouseenter = function(){
+		view.focus();
+	};
+	view.onmouseleave = function(){
+		view.blur();
+	};
 }
 
 function initViewWheelListener(viewConfig, type, viewsConfig, cube) {
@@ -251,7 +231,7 @@ function initViewWheelListener(viewConfig, type, viewsConfig, cube) {
 		let scale = event.deltaY > 0 ? 1.1 : 0.9;
 		viewConfig.zoomRatio *= scale;
 		updateThreeCameraByView(viewsConfig, cube);
-		// updateViewLines(viewConfig, boxScale);
+		updateViewLines(viewsConfig, cube);//更新三视图线段位置
 	}
 }
 
@@ -274,15 +254,14 @@ function updateViewLines(viewsConfig, {scale}) {
 		{
 			viewBoxWidth = view.width * 2/3 / view.zoomRatio;
 			viewBoxHeight = viewBoxWidth/boxRatio;
-			console.log(view.width, view.zoomRatio, viewBoxWidth, viewBoxHeight, 'aaaaa');
 		} else {
 			viewBoxHeight = view.height * 2/3 / view.zoomRatio;
 			viewBoxWidth = viewBoxHeight * boxRatio 
-			console.log(scale, boxRatio, view, view.zoomRatio, view.height, viewBoxWidth, viewBoxHeight, 'bbb');
 		}
 		const viewCenterWidth = view.width/2;
 		const viewCenterHeight = view.height/2;
-		// 基于左上角顶点(0,0)
+		view.viewBoxWidth = viewBoxWidth;
+		view.viewBoxHeight = viewBoxHeight;
 		const left = (viewCenterWidth - viewBoxWidth/2); //纯数字就是绝对值像素，或者带%的相对值
 		const right = (viewCenterWidth + viewBoxWidth/2);
 		const top = (viewCenterHeight - viewBoxHeight/2);
@@ -307,6 +286,7 @@ function updateLinePosition(view, left, right, top, bottom) {
 	lineRight.setAttribute('y2', '100%');//bottom
 
 	const lineTop = document.getElementById(view.type+'LineTop');
+
 	lineTop.setAttribute('x1', '0%');//left
 	lineTop.setAttribute('y1', top);
 	lineTop.setAttribute('x2', '100%');//right
@@ -378,31 +358,293 @@ function updateHandlePosition(view, left, right, top, bottom) {
 
 	
 	const handleDirection = document.getElementById(view.type+'DirectionHandle');
-	handleDirection.setAttribute('x', (left+right)/2-10);
+	handleDirection.setAttribute('x', (left+right)/2-10);//矩阵的左顶点
 	handleDirection.setAttribute('y', "0%"); 
 	handleDirection.setAttribute('height', Math.max((bottom-top)/2-10+top,0));//-10防止和移动区域重叠
 }
-function initHandleListener(handle, lineArr) {
-	handle.onmouseenter = function(){ highlightLine(lineArr) };    
-	handle.onmouseleave = function(){ hideLine(lineArr)};
-	handle.onmousedown = function(event){
-		event.stopPropagation();
-		highlightLine(lineArr);
+function initHandleListener(svg, handle, linesArr, type, direction, viewConfig, cube, viewsConfig) {
+	handle.onmouseenter = function(){ highlightLine(linesArr) };    
+	handle.onmouseleave = function(){ hideLine(linesArr)};
+
+	handle.onmousedown = function(){
+		highlightLine(linesArr);
 		handle.onmouseleave = null;
+		let startPos = {};
+		let endPos = {}
+		let viewCenterX = viewConfig.width / 2;
+		let viewCenterY = viewConfig.height / 2;
+		let theta = 0;
+		let ratio = {x:0, y:0};
+		svg.onmousedown = function(event) {
+			event.stopPropagation();
+			startPos.x = event.clientX - (viewCenterX + viewConfig.leftDistance);// - (viewCenterX + viewConfig.leftDistance)
+			startPos.y = event.clientY - (viewCenterY + viewConfig.topDistance);
+		}
+		svg.onmouseup = function(event) {
+			// event.stopPropagation();
+			svg.onmousemove = null;
+			svg.onmouseup=null;
+			hideLine(linesArr);
+			handle.onmouseenter = function(){ highlightLine(linesArr) };
+			handle.onmouseleave = function(){ hideLine(linesArr) } ;
+			if (type === 'direction') {
+				if (theta !== 0) {
+					directionChanged(viewConfig.type, theta,cube);
+				}
+			} else if (type === 'move') {
+				moveChanged(viewConfig.type, ratio, cube);
+			} else if (type === 'edge') {
+				edgeChanged(viewConfig.type, ratio, cube, direction);
+			}
+			threeCameraLookAtCube(viewsConfig, cube);
+			updateThreeCameraByView(viewsConfig, cube);
+			updateViewLines(viewsConfig, cube);//更新三视图线段位置
+		}
+		svg.onmousemove = function(event) {
+			endPos.x = event.clientX  - (viewCenterX + viewConfig.leftDistance);
+			endPos.y = event.clientY  - (viewCenterY + viewConfig.topDistance);
+			if(type === 'direction') {
+				theta = Math.atan2(endPos.y,  endPos.x) - Math.atan2(startPos.y,  startPos.x);
+				rotateLines(viewConfig, theta, linesArr);
+			} else {
+				ratio.x = (endPos.x - startPos.x) / viewConfig.width;// 移动的距离像素/当前视口的宽度像素
+				ratio.y = - (endPos.y - startPos.y) / viewConfig.height;// 移动的距离像素/当前视口的高度像素
+				moveLines(viewConfig, {
+					x:(endPos.x - startPos.x),
+					y:(endPos.y - startPos.y)
+				},direction);
+			}
+	
+		}
+	}
+	handle.onmouseup = function() {
+		hideLine(linesArr);
+		handle.onmouseleave = hideLine;
+	};
+
+	
+}
+function directionChanged(type, theat, cube) {
+	console.log(type, cube, 'cube');
+	if (type === 'top') {
+		cube.quaternion.multiply(new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 0, 1, 0 ), theat ) ).normalize();
+	} else if (type === 'right') {
+		cube.quaternion.multiply(new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 1, 0, 0 ), theat ) ).normalize();
+	} else {
+		cube.quaternion.multiply(new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3( 0, 0, 1 ), -theat ) ).normalize();
+	}
+	
+}
+function moveChanged(type, ratio, cube) {
+	if (type === 'top') {
+		let distanceX = ratio.x* cube.scale.x;
+		let distanceY = ratio.y* cube.scale.z;
+		translatePosition(distanceX, 'x', cube, type);
+		translatePosition(distanceY, 'z', cube, type);
+	} else if (type === 'right') {
+		let distanceX = ratio.x* cube.scale.z;
+		let distanceY = ratio.y* cube.scale.y;
+		translatePosition(distanceX, 'z', cube, type);
+		translatePosition(distanceY, 'y', cube, type);
+
+	} else if (type === 'front') {
+		let distanceX = ratio.x* cube.scale.x;
+		let distanceY = ratio.y* cube.scale.y;
+		translatePosition(distanceX, 'x', cube, type);
+		translatePosition(distanceY, 'y', cube, type);
 	}
 }
+function edgeChanged(type, ratio, cube, direction) {
+	if (type === 'top') {
+		let distanceX = cube.scale.x*ratio.x*direction.x;
+		let distanceY = cube.scale.z*ratio.y*direction.y;
+		translatePosition(distanceX/2, 'x', cube, type);
+		translatePosition(distanceY/2, 'z', cube, type);
+		cube.scale.x += distanceX;
+		cube.scale.z += distanceY;
+	} else if (type === 'right') {
+		let distanceX = cube.scale.z*ratio.x*direction.x;
+		let distanceY = cube.scale.y*ratio.y*direction.y;
+		translatePosition(distanceX/2, 'z', cube, type);
+		translatePosition(distanceY/2, 'y', cube, type);
+		cube.scale.z += distanceX;
+		cube.scale.y += distanceY;
+	} else {
+		let distanceX = cube.scale.x*ratio.x*direction.x;
+		let distanceY = cube.scale.y*ratio.y*direction.y;
+		translatePosition(distanceX/2, 'x', cube, type);
+		translatePosition(distanceY/2, 'y', cube, type);
+		cube.scale.x += distanceX;
+		cube.scale.y += distanceY;
+	}
+}
+function translatePosition(distance, axis, cube) {
 
-function highlightLine(lineArr){
-	for (let i = 0; i < lineArr.length; i++){
-		const line = lineArr[i];
+	const {quaternion} = cube;
+	let direction;
+	if(axis === 'z') {
+		direction = new THREE.Vector3(0,0,1).applyQuaternion(quaternion).normalize();
+	} else if(axis === 'y') {
+		direction = new THREE.Vector3(0,1,0).applyQuaternion(quaternion).normalize();
+	} else if(axis === 'x') {
+		direction = new THREE.Vector3(1,0,0).applyQuaternion(quaternion).normalize();
+	}
+	cube.position.addScaledVector(direction, distance)
+}
+
+
+// 线段顶点绕视图中心点旋转后的坐标
+function rotateLines(viewConfig, angle, linesArr) {
+	let viewCenterX = viewConfig.width / 2;
+	let viewCenterY = viewConfig.height / 2;
+	const rm = new THREE.Matrix4().set(
+		Math.cos(angle), -Math.sin(angle), 0, 0,
+		Math.sin(angle),  Math.cos(angle), 0, 0,
+		0,  0, 1, 0,
+		0,  0, 0, 1
+	);
+	rm.setPosition(viewCenterX,viewCenterY,0);
+	handleAfterRotateLinePosition(viewConfig, linesArr, rm);
+
+}
+function moveLines(viewConfig,delta, direction) {
+	const viewCenterWidth = viewConfig.width/2;
+	const viewCenterHeight = viewConfig.height/2;
+	let left = (viewCenterWidth - viewConfig.viewBoxWidth/2); 
+	let right = (viewCenterWidth + viewConfig.viewBoxWidth/2);
+	let top = (viewCenterHeight - viewConfig.viewBoxHeight/2);
+	let bottom = (viewCenterHeight + viewConfig.viewBoxHeight/2);
+	if (direction) {
+		if (direction.x === 1){ //right
+			right += delta.x;
+		} else if (direction.x === -1){ //left
+			left += delta.x;
+		}
+		if (direction.y === -1){ //bottom
+			bottom += delta.y;
+		} else if (direction.y === 1){ //top
+			top += delta.y;
+		}
+	} else {
+		left += delta.x;
+		top += delta.y;
+		right += delta.x;
+		bottom += delta.y;  
+	}
+	updateLinePosition(viewConfig, left, right, top, bottom);
+
+}
+
+function handleAfterRotateLinePosition(viewConfig, linesArr, rm) {
+	
+		// 1. 基于视图原点，物体左边线的两个顶点
+		let line = linesArr[0];
+		let point1 = new THREE.Vector3(-viewConfig.viewBoxWidth/2, -viewConfig.height/2, 1);
+		let point2 = new THREE.Vector3(-viewConfig.viewBoxWidth/2,  viewConfig.height/2, 1);
+		setLinePositionByRotate(point1, point2,rm, line,viewConfig);
+
+		// 2. 基于视图原点，物体右边线的两个顶点
+		line = linesArr[1];
+		point1 = new THREE.Vector3(viewConfig.viewBoxWidth/2, -viewConfig.height/2, 1);
+		point2 = new THREE.Vector3(viewConfig.viewBoxWidth/2,  viewConfig.height/2, 1);
+		setLinePositionByRotate(point1, point2,rm, line);
+
+		// 3. 基于视图原点，物体顶边线的两个顶点
+		line = linesArr[2];
+		point1 = new THREE.Vector3(-viewConfig.width/2,  viewConfig.viewBoxHeight/2, 1);
+		point2 = new THREE.Vector3(viewConfig.width/2,  viewConfig.viewBoxHeight/2, 1);
+		setLinePositionByRotate(point1, point2,rm, line);
+
+		// 4. 基于视图原点，物体底边线的两个顶点
+		line = linesArr[3];
+		point1 = new THREE.Vector3(-viewConfig.width/2,  -viewConfig.viewBoxHeight/2, 1);
+		point2 = new THREE.Vector3(viewConfig.width/2,  -viewConfig.viewBoxHeight/2, 1);
+		setLinePositionByRotate(point1, point2,rm, line);
+
+	    // 5. 基于视图原点，物体方向的第二个顶点
+		line = linesArr[4];
+		point2 = new THREE.Vector3(0,  -viewConfig.height/2, 1);
+		let newPoint2 = new THREE.Vector3().copy(point2).applyMatrix4(rm);
+		line.setAttribute("x2", newPoint2.x);
+		line.setAttribute("y2", newPoint2.y);
+	
+	
+}
+
+function setLinePositionByRotate(point1, point2,rm, line) {
+	let newPoint1 = new THREE.Vector3().copy(point1).applyMatrix4(rm);
+	let newPoint2 = new THREE.Vector3().copy(point2).applyMatrix4(rm);
+	line.setAttribute("x1", newPoint1.x);
+	line.setAttribute("y1", newPoint1.y);
+	line.setAttribute("x2", newPoint2.x);
+	line.setAttribute("y2", newPoint2.y);
+}
+
+function highlightLine(linesArr){
+	for (let i = 0; i < linesArr.length; i++){
+		const line = linesArr[i];
 		line.style.stroke="yellow";
 	}
 }
-function hideLine(lineArr){
-	for (let i = 0; i < lineArr.length; i++){
-		const line = lineArr[i];
+function hideLine(linesArr){
+	for (let i = 0; i < linesArr.length; i++){
+		const line = linesArr[i];
 		line.style.stroke="#00000000";
 	}
 }
+function onKeyDown(event,type, cube,viewsConfig){
 
+	switch(event.key){
+		case 'e':
+			event.preventDefault();
+			event.stopPropagation();
+			directionChanged(type, -0.005, cube);
+			threeCameraLookAtCube(viewsConfig, cube);
+			updateThreeCameraByView(viewsConfig, cube);
+			updateViewLines(viewsConfig, cube);
+			break;
+		case 'q':
+			event.preventDefault();
+			event.stopPropagation();
+			directionChanged(type, 0.005, cube);
+			threeCameraLookAtCube(viewsConfig, cube);
+			updateThreeCameraByView(viewsConfig, cube);
+			updateViewLines(viewsConfig, cube);
+			break;
+		case 'w':
+		case 'ArrowUp':
+			event.preventDefault();
+			event.stopPropagation();
+			moveChanged(type, {x:0, y:0.01}, cube);
+			threeCameraLookAtCube(viewsConfig, cube);
+			updateThreeCameraByView(viewsConfig, cube);
+			updateViewLines(viewsConfig, cube);
+			break;
+		case 's':
+		case 'ArrowDown':
+			event.preventDefault();
+			event.stopPropagation();
+			moveChanged(type, {x:0, y:-0.01}, cube);
+			threeCameraLookAtCube(viewsConfig, cube);
+			updateThreeCameraByView(viewsConfig, cube);
+			updateViewLines(viewsConfig, cube);
+			break;
+		case 'a':
+		case 'ArrowLeft':
+			moveChanged(type, {x:-0.01, y:0}, cube);
+			threeCameraLookAtCube(viewsConfig, cube);
+			updateThreeCameraByView(viewsConfig, cube);
+			updateViewLines(viewsConfig, cube);
+			break;
+		case 'd':
+		case 'ArrowRight':
+			event.preventDefault();
+			event.stopPropagation();
+			moveChanged(type, {x:0.01, y:0}, cube);
+			threeCameraLookAtCube(viewsConfig, cube);
+			updateThreeCameraByView(viewsConfig, cube);
+			updateViewLines(viewsConfig, cube);
+			break;
+	}
+}
 export {ThreeViews}
